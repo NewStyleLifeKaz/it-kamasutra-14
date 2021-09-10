dfc89a779d16d3dc3a1f9040065d8df61725a24a
NewStyleLifeKaz <Nursultan.Dzhumanov@gmail.com>
2021-09-11 00:38:25 +0300

    it-56-doubleComponentContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,85 +1,46 @@
 import style from "./style.module.css";
-import * as axios from "axios";
 import userPhotos from "../../assets/image/users.png";
-import React from "react";
 
-
-class Users extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// }
-
-	// props.setUsers([
-	// 	{
-	// 		id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
-	// 		followed: true, fullName: 'Dmitry', status: 'I am so pretty', location: { city: 'Minsk', country: 'Belarus' }
-	// 	},
-	// 	{
-	// 		id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
-	// 		followed: false, fullName: 'Andrey', status: 'Yo bro', location: { city: 'Moscow', country: 'Russia' }
-	// 	},
-	// 	{
-	// 		id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
-	// 		followed: true, fullName: 'Sveta', status: 'So cool', location: { city: 'Kiev', country: 'Ukraine' }
-	// 	},
-	// ])
-	componentDidMount() {
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-			.then(response => {
-				this.props.setUsers(response.data.items);
-				this.props.setTotalUsersCount(response.data.totalCount);
-			});
+let Users = (props) => {
+	let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	let pages = [];
+	for (let i = 1; i <= pageCount; i++) {
+		pages.push(i);
 	}
-	onCurrentPage = (pageNumber) => {
-		this.props.setCurrentPage(pageNumber);
-		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-			.then(response => {
-				this.props.setUsers(response.data.items);
-			});
-	}
-
-	render() {
-		let pageCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-		let pages = [];
-		for (let i = 1; i <= pageCount; i++) {
-			pages.push(i);
-		}
-
-		return <div>
-			<div>
-				<span className={style.point}>
-					{pages.map(p => {
-						return <span className={this.props.currentPage === p && style.selectedPage}
-							onClick={() => { this.onCurrentPage(p) }}>{p}</span>
-					})}
+	return <div>
+		<div>
+			<span className={style.point}>
+				{pages.map(p => {
+					return <span className={props.currentPage === p && style.selectedPage}
+						onClick={() => { props.onCurrentPage(p) }}>{p}</span>
+				})}
+			</span>
+		</div>
+		{
+			props.users.map(u => <div key={u.id}>
+				<span>
+					<div>
+						<img src={u.photos.small != null ? u.photos.small : userPhotos} className={style.userPhoto} />
+					</div>
+					<div>
+						{u.followed
+							? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
+							: <button onClick={() => { props.follow(u.id) }} >Follow</button>
+						}
+					</div>
 				</span>
-			</div>
-			{
-				this.props.users.map(u => <div key={u.id}>
+				<span>
 					<span>
-						<div>
-							<img src={u.photos.small != null ? u.photos.small : userPhotos} className={style.userPhoto} />
-						</div>
-						<div>
-							{u.followed
-								? <button onClick={() => { this.props.unfollow(u.id) }} >Unfollow</button>
-								: <button onClick={() => { this.props.follow(u.id) }} >Follow</button>
-							}
-						</div>
+						<div>{u.name} </div>
+						<div>{u.status} </div>
 					</span>
 					<span>
-						<span>
-							<div>{u.name} </div>
-							<div>{u.status} </div>
-						</span>
-						<span>
-							<div>{'u.location.country'} </div>
-							<div>{'u.location.city'} </div>
-						</span>
+						<div>{'u.location.country'} </div>
+						<div>{'u.location.city'} </div>
 					</span>
-				</div >)
-			}
-		</div >
-	}
+				</span>
+			</div >)
+		}
+	</div >
 };
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,53 @@
+import React from "react";
 import { connect } from "react-redux";
 import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import * as axios from "axios";
 import Users from "./Users";
+
+class UsersContainer extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+	// }
+
+	// props.setUsers([
+	// 	{
+	// 		id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
+	// 		followed: true, fullName: 'Dmitry', status: 'I am so pretty', location: { city: 'Minsk', country: 'Belarus' }
+	// 	},
+	// 	{
+	// 		id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
+	// 		followed: false, fullName: 'Andrey', status: 'Yo bro', location: { city: 'Moscow', country: 'Russia' }
+	// 	},
+	// 	{
+	// 		id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf-YY4QxFp_OK4ytMjHDRKl-SYDDDEDZk7jw&usqp=CAU',
+	// 		followed: true, fullName: 'Sveta', status: 'So cool', location: { city: 'Kiev', country: 'Ukraine' }
+	// 	},
+	// ])
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+			.then(response => {
+				this.props.setUsers(response.data.items);
+				this.props.setTotalUsersCount(response.data.totalCount);
+			});
+	}
+	onCurrentPage = (pageNumber) => {
+		this.props.setCurrentPage(pageNumber);
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+			.then(response => {
+				this.props.setUsers(response.data.items);
+			});
+	}
+
+	render() {
+		return <Users totalUsersCount={this.props.totalUsersCount}
+			pageSize={this.props.pageSize}
+			currentPage={this.props.currentPage}
+			onCurrentPage={this.onCurrentPage}
+			users={this.props.users}
+			unfollow={this.props.unfollow}
+			follow={this.props.follow} />
+	}
+};
 
 let mapStateToProps = (state) => {
 	return {
@@ -32,4 +79,4 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
