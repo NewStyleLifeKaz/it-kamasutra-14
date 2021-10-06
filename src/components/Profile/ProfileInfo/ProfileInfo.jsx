e36910db752b136bcdf5025bd893396d8b691927
NewStyleLifeKaz <Nursultan.Dzhumanov@gmail.com>
2021-10-06 19:42:36 +0300

    it71-setState-Bind-Blur-autoFocus-localState-2

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
 	if (!props.profile) {
@@ -7,12 +8,12 @@ const ProfileInfo = (props) => {
 	}
 	return (
 		<div>
-			<div className={s.item}>
+			{/* <div className={s.item}>
 				<img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="" />
-			</div>
+			</div> */}
 			<div className={s.descriptionBlock}>
 				<img src={props.profile.photos.large} />
-				ava + description
+				<ProfileStatus status={'Hey wanna be my friend?!'} />
 			</div>
 		</div>
 	);
