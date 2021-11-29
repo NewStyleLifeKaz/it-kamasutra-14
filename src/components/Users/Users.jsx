4e1691c436d2ae17c8158c80e2fd1b322152f8e5
NewStyleLifeKaz <Nursultan.Dzhumanov@gmail.com>
2021-11-29 18:21:20 +0300

    it91-chrome-extension

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,8 @@
-import style from "./style.module.css";
-import userPhotos from "../../assets/image/users.png";
-import { NavLink } from "react-router-dom";
 import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
 
 let Users = ({ currentPage, onCurrentPage, users, totalUsersCount, pageSize, ...props }) => {
-
+	//lesson 90
 	return <div>
 		{/* <div>
 			<span className={style.point}>
@@ -15,35 +13,16 @@ let Users = ({ currentPage, onCurrentPage, users, totalUsersCount, pageSize, ...
 			</span>
 		</div> */}
 		<Paginator currentPage={currentPage} onCurrentPage={onCurrentPage} totalUsersCount={totalUsersCount} pageSize={pageSize} />
-		{
-			users.map(u => <div key={u.id}>
-				<span>
-					<div>
-						<NavLink to={'/profile/' + u.id}>
-							<img src={u.photos.small != null ? u.photos.small : userPhotos} className={style.userPhoto} />
-						</NavLink>
-					</div>
-					<div>
-						{u.followed
-							? <button disabled={props.followingInProgress.some(id => id === u.id)}
-								onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
-							: <button disabled={props.followingInProgress.some(id => id === u.id)}
-								onClick={() => { props.follow(u.id) }} >Follow</button>
-						}
-					</div>
-				</span>
-				<span>
-					<span>
-						<div>{u.name} </div>
-						<div>{u.status} </div>
-					</span>
-					<span>
-						<div>{'u.location.country'} </div>
-						<div>{'u.location.city'} </div>
-					</span>
-				</span>
-			</div>)
-		}
+		<div>
+			{
+				users.map(u => <User user={u}
+					followingInProgress={props.followingInProgress}
+					unfollow={props.unfollow}
+					follow={props.follow}
+					key={u.id} />
+				)
+			}
+		</div>
 	</div>
 };
 export default Users;
